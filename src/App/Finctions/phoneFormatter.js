dbf14680b6c6155f93d4784b248e2eb124a320eb
GraN <117268180+nagraiv@users.noreply.github.com>
2024-06-02 10:12:38 +0700

    second task is done

---
+++ src/App/Finctions/phoneFormatter.js
@@ -0,0 +1,11 @@
+
+export default function phoneFormatter(inputPhone) {
+    const filteredPhone = inputPhone.replace(/[- \(\)]/g, '');
+    if (/[^\+]\D/.test(filteredPhone)) {
+        throw new Error('Посторонние символы в номере телефона!');
+    }
+    if (!/^(8|\+\d)\d{10,}/.test(filteredPhone)) {
+        throw new Error('В номере телефона должно быть больше цифр!');
+    }
+    return filteredPhone.replace(/^8/, '+7');
+}
--- src/App/app.js
+++ src/App/app.js
@@ -1,13 +1,18 @@
 import Validator from "./Validator/Validator";
+import phoneFormatter from "./Finctions/phoneFormatter";
+
 console.log('application started');
 
-console.log(Validator.validateUsername('L'));
-console.log(Validator.validateUsername('Li'));
-console.log(Validator.validateUsername('1L'));
-console.log(Validator.validateUsername('L-'));
-console.log(Validator.validateUsername('A-r'));
-console.log(Validator.validateUsername('c-R'));
-console.log(Validator.validateUsername('rhugweb______nklgjwbnl'));
-console.log(Validator.validateUsername('s123f'));
-console.log(Validator.validateUsername('f1234h'));
 console.log(Validator.validateUsername('nj12hj34b-hj456b'));
+
+console.log(phoneFormatter('8 (927) 000-00-00'));
+
+console.log(phoneFormatter('+7 960 000 00 00'));
+
+console.log(phoneFormatter('+86 000 000 0000'));
+
+try {
+    console.log(phoneFormatter('+55555'));
+} catch (e) {
+    console.error(e.message);
+}
